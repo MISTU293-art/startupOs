e1715b60d824702ea909eae663a10c59f08bc1fa
Mistu293-art <mistu14112006@gmail.com>
2026-07-30 01:00:38 +0530

    staff update route addedd and controller addedd

--- src/controllers/staff.controller.js
+++ src/controllers/staff.controller.js
@@ -41,7 +41,7 @@ async function registerStaff(req, res) {
     console.log(employee);
     return res.status(201).json({
       message: "Staff Registered",
-      employee
+      employee,
     });
   } catch (error) {
     console.log(error);
@@ -50,21 +50,59 @@ async function registerStaff(req, res) {
     });
   }
 }
-async function staffDetails(req,res) {
-  try{
-   const employee = await StaffModel
-    .find()
-    .populate("hiredBy", "username fullName");
+async function staffDetails(req, res) {
+  try {
+    const employee = await StaffModel.find().populate(
+      "hiredBy",
+      "username fullName",
+    );
     return res.status(200).json({
-      message:"All Staffs Fetched SuccessFully",
-      employee
-    })
-  }
-  catch(error){
+      message: "All Staffs Fetched SuccessFully",
+      employee,
+    });
+  } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message:"Internal Server Error"
-    })
+      message: "Internal Server Error",
+    });
   }
 }
-export { registerStaff ,staffDetails};
+
+async function updateStaff(req, res) {
+  try {
+    const _id = req.params._id;
+    const { status, address, qualification, salary } = req.body;
+
+    const user = await StaffModel.findOne({ _id });
+    console.log(user)
+    if (!user) {
+      return res.status(404).json({
+        message: "Staff Not Found",
+      });
+    }
+
+    const updatedData = await StaffModel.findByIdAndUpdate(
+      _id,
+      {
+        status,
+        address,
+        qualification,
+        salary,
+      },
+      {
+        new: true,
+      },
+    );
+
+    return res.status(200).json({
+      message:"Staff Details Updated",
+      updatedData
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+export { registerStaff, staffDetails ,updateStaff};
--- src/routes/staff.route.js
+++ src/routes/staff.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { Route } from "express";
-import { registerStaff,staffDetails } from "../controllers/staff.controller.js";
+import { registerStaff,staffDetails,updateStaff } from "../controllers/staff.controller.js";
 import {isLoggedIn, isUserAdminAndHrAndAccounts,isUserHrandAdmin} from '../middlewares/auth.middleware.js';
 const router = express.Router();
 /**POST /staff/register */
 router.post('/register',isLoggedIn,isUserHrandAdmin,registerStaff);
 /**GET /staff/details */
-router.get('/details',isLoggedIn,isUserAdminAndHrAndAccounts,staffDetails)
+router.get('/details',isLoggedIn,isUserAdminAndHrAndAccounts,staffDetails);
+/** PATCH /staff/update/:_id */
+router.patch('/update/:_id',isLoggedIn,isUserHrandAdmin,updateStaff);
 export default router
